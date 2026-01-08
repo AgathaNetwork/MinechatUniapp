8be1adb2f3eacc4f331d82a3308ffcb699f87cc3
MikeWu597 <i@hyp.ink>
2026-01-09 00:57:36 +0800

    first commit

---
+++ minechat-uniapp/src/pages/webview/webview.vue
@@ -0,0 +1,16 @@
+<template>
+	<web-view :src="url" />
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			url: 'https://front-dev.agatha.org.cn/'
+		}
+	}
+}
+</script>
+
+<style>
+</style>
